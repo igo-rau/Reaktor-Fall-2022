7dd9b1d856dc2fb4f1ec349b0e7aeb7633fb5b95
Igor Rautiainen <igor.rautiainen@outlook.com>
2022-05-25 19:57:09 +0300

    Separate class Packages introduced

---
+++ ReaktorAssignment/src/Packages.java
@@ -0,0 +1,36 @@
+
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class Packages {
+    private SortedMap<String, Package> installedPackages;
+
+    public Packages() {
+        installedPackages = new TreeMap<>();
+    }
+
+    public void addPackage(String name, Package pack) {
+        this.installedPackages.put(name, pack);
+    }
+
+    public Set<String> getKeySet() {
+        return installedPackages.keySet();
+    }
+
+    public Package getPack(String packName) {
+        return installedPackages.get(packName);
+    }
+
+    public boolean containsPack(String packname) {
+        if (installedPackages.containsKey(packname)) {
+            return true;
+        }
+        return false;
+    }
+
+    public Set<Map.Entry<String,Package>> getEntrySet() {
+        return this.installedPackages.entrySet();
+    }
+}
